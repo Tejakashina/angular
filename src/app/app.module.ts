8f0a7fcf1bd68cc4759204c89b9a20ccf3750c32
Tejakashina <kashinatejasri22@gmail.com>
2022-02-02 10:37:39 +0530

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { PatientformComponent } from './patientform/patientform.component';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import { SearchPipe } from './search.pipe';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientformComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
